722bc83bc75c2803eb822444b39d3e2d88ae5070
Anantha Kumaran <ananthakumaran@gmail.com>
2026-08-02 11:34:04 +0530

    lint

--- pimacs-markdown-table.el
+++ pimacs-markdown-table.el
@@ -15,6 +15,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Markdown table rendering helpers.
+
 ;;; Code:
 
 (require 'cl-lib)
